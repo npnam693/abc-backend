f33fe782f6d5f0d42447b7ed5667b3af0ea3b216
Nguyen Phi Nam <nguyenphinam2k2@gmail.com>
2023-11-11 15:22:20 +0700

    final commit

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -3,18 +3,12 @@ const router = express.Router();
 
 const projectController = require('../app/controllers/ProjectController');
 
-// http:localhost:3000/project/:slug
 router.get('/project-unis', projectController.getAllProjectUni);
 router.put('/:slug/isChecked', projectController.updateIsChecked);
 router.get('/:slug', projectController.get);
-// http:localhost:3000/project
 router.get('/', projectController.getAll);
-// http:localhost:3000/project
 router.post('/', projectController.create);
-// http:localhost:3000/project/:slug/isChecked
-// http:localhost:3000/project/:slug
 router.put('/', projectController.update);
-// http:localhost:3000/project/:slug
 router.delete('/:slug', projectController.delete);
 
 module.exports = router;
